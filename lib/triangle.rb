a6e36212dc636e767260d4bcb9ab4f2ad7dd9228
Sean Langton <slangton1@gmail.com>
2018-11-13 17:36:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,7 +4,7 @@ attr_accessor :kind
 
   def initialize (side_a,side_b,side_c)
     if side_a * side_b * side_c == 0 then raise TriangleError
-    if side_a < 0 || side_b < 0 || side_c < 0 then raise TriangleError
+    elsif side_a < 0 || side_b < 0 || side_c < 0 then raise TriangleError
     elsif side_a == side_b && side_b == side_c then @kind = :equilateral
     elsif side_a == side_b || side_b == side_c || side_a == side_c then @kind = :isosceles
     else @kind = :scalene
