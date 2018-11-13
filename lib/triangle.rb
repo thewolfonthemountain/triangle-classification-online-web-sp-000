abe97471144321217d046cedd8f014641edf68cf
Sean Langton <slangton1@gmail.com>
2018-11-13 17:31:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,7 +4,7 @@ attr_accessor :kind
 
   def initialize (side_a,side_b,side_c)
     @kind = :equilateral if side_a == side_b && side_b == side_c
-    @kind = :isosceles if side_a == side_b || side_b == side_c || side_a == side_c
+
   end
 
 
